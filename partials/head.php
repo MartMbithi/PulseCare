060073bff92b393984162d9fd7d6a575f0e4a54e
Mart Mbithi <martdevelopers254@gmail.com>
2024-09-19 11:12:00 +0300

    Partialize Assets

---
+++ partials/head.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html
+    lang="en"
+    class="light-style layout-menu-fixed"
+    dir="ltr"
+    data-theme="theme-default"
+    data-assets-path="../public/"
+    data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <title>PulseCare - Improving Patient Experience Through Realtime Feedbacks</title>
+
+    <meta name="description" content="" />
+    <link rel="icon" type="image/x-icon" href="../public/img/favicon/favicon.ico" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
+    <link rel="stylesheet" href="../public/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="../public/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../public/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../public/css/demo.css" />
+    <link rel="stylesheet" href="../public/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="../public/vendor/libs/apex-charts/apex-charts.css" />
+    <script src="../public/vendor/js/helpers.js"></script>
+    <script src="../public/js/config.js"></script>
+</head>
---
+++ partials/scripts.php
@@ -0,0 +1,8 @@
+<script src="../public/vendor/libs/jquery/jquery.js"></script>
+<script src="../public/vendor/libs/popper/popper.js"></script>
+<script src="../public/vendor/js/bootstrap.js"></script>
+<script src="../public/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="../public/vendor/js/menu.js"></script>
+<script src="../public/vendor/libs/apex-charts/apexcharts.js"></script>
+<script src="../public/js/main.js"></script>
+<script src="../public/js/dashboards-analytics.js"></script>
