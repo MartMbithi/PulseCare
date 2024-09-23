abdf2561bd833c64cb9fdecb7b9b582dbe387677
Mart Mbithi <martdevelopers254@gmail.com>
2024-09-23 11:31:06 +0300

    Craft Profile Pages

--- partials/aside.php
+++ partials/aside.php
@@ -62,7 +62,7 @@
 
     <ul class="menu-inner py-1">
         <!-- Dashboard -->
-        <li class="menu-item active">
+        <li class="menu-item">
             <a href="home" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-home-circle"></i>
                 <div data-i18n="Analytics">Home</div>
